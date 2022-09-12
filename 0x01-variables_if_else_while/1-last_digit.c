e09b63ac379a7c621bbc9faea1707bf094ee716b
Jimoh Yusuf <jyusuf2188@gmail.com>
2022-09-12 11:24:10 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -29,7 +29,7 @@ else if (l == 0)
 }
 else
 {
-	printf("Last digit of %d is %d nd is less than 6 and not 0\n", n, l);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
