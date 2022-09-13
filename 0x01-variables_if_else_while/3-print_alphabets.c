c7d93165a1ef00634833ff38f3d414c7e08f9e7c
Jimoh Yusuf <jyusuf2188@gmail.com>
2022-09-12 17:47:48 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,9 +2,9 @@
 /**
  * main - print if the number is positive, zero, or negative
  *
- * Description: using the main function, this program prints
- * "Programming is positive, zero, or negative."
- * Returnn: 0
+ * Description: using the main function, this program 
+ * prints "Programming is positive, zero, or negative."
+ * Return: 0
  */
 int main(void)
 {
