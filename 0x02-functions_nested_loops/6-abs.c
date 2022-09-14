1931472396afcdca821a1a015616429e167b5af6
Jimoh Yusuf <jyusuf2188@gmail.com>
2022-09-14 05:27:06 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - entry point
+ *
+ * @n: is the int that will be used as argument
+ * Description: using the _abs function, the program
+ * will compute the absolute value of n
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n > 0 || n == 0)
+	{
+		return (n);
+	}
+	else
+		return (c * -1);
+}
+
