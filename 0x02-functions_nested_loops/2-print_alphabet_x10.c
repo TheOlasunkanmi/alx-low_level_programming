cf0267e2e09286e5c90d599f51255dcbdb66e39b
Jimoh Yusuf <jyusuf2188@gmail.com>
2022-09-14 03:49:39 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: using the print_alphabet_x10 function, the program prints
+ * abc.. ten times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; 1++)
+	{
+		for (c = 'a'; c <= 'z'; c+=)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
