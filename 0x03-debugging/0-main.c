75433a3a50c50f04dee5869698233d0a4329ed0d
Jimoh Yusuf <jyusuf2188@gmail.com>
2022-09-15 07:54:01 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - entry point
  *
@@ -7,14 +6,11 @@
  * is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
-
