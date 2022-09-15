61aa86ef16985d2b32b3451126943bfd09b10851
Jimoh Yusuf <jyusuf2188@gmail.com>
2022-09-15 02:55:23 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - entry point
+ *
+ * @x: the first integer
+ * @y: the second integer
+ * Description: using the add function, the program adds two
+ * integer and return the result
+ * Return: result
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
