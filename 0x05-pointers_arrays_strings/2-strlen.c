399b64e2df5bda05ded7b46e0eba14083994e724
Jimoh Yusuf <jyusuf2188@gmail.com>
2022-09-22 08:36:08 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ *
+ * Description: the progam returns the length of a string
+ *
+ * @s: the input parameter
+ *
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+	return (index);
+}
