f67a9f384550e75133a8a2edbf3153692bc88e56
Jimoh Yusuf <jyusuf2188@gmail.com>
2022-10-13 17:28:07 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 
-
 /**
  * int_index - searches for an intger.
  *
@@ -18,9 +17,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
+		if(size <= 0)
+			return (-1);
+
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]) != 0)
+			if (cmp(array[i]))
 				return (i);
 		}
 	}
